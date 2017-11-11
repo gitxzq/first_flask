0bfc1c936238c3d56201f66700f5859d5aea73ff
xzq <x7652514@163.com>
2017-11-11 16:10:32 +0800

    flask-5a

--- hello.py
+++ hello.py
@@ -5,14 +5,46 @@ from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+import os
 
+basedir=os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+
+db=SQLAlchemy(app)
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+
+
+# 定义Role和User模型
+# 类变量 __tablename__ 定义在数据库中使用的表名。如果没有定义 __tablename__,Flask-SQLAlchemy 会使用一个默认名字,
+# 但默认的表名没有遵守使用复数形式进行命名的约定, 所以最好由我们自己来指定表名。
+# 其余的类变量都是该模型的属性,被定义为 db.Column 类的实例。
+
+
+class Role(db.Model):
+    __tablename__='roles'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(64),unique=True)
+    users=db.relationship('User',backref='role',lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>'% self.name
+
+class User(db.Model):
+    __tablename__='users'
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(64),unique=True,index=True)
+    role_id=db.Column(db.Integer,db.foreign('role_id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
